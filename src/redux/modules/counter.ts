cc38ddc44232b7db3a1351386b21e1af2976fe24
parksubeom <sooknise@naver.com>
2025-09-17 15:14:48 +0900

    feat : redux 카운터 구현 및 dispatch,reducer,store 설정

--- src/redux/modules/counter.ts
+++ src/redux/modules/counter.ts
@@ -1,12 +1,12 @@
 // src/modules/counter.js
 
-// 초기 상태값
+// 초기 상태값 -> 카운터를 만들거기 떄문에 값의 타입은 넘버
 const initialState = {
   number: 0,
 };
 
 // 액션 타입 상수를 정의합니다.
-// `as const`를 사용하면 타입이 string이  리터럴로 고정 -> 리터럴이 아니면 string 타입도 추론으로 허용되어버림
+// `as const`를 사용하면 타입이 string에서 리터럴로 변경 -> 리터럴이 아니면 string 타입도 추론으로 허용되어버림
 const INCREMENT = "INCREMENT" as const; 
 const DECREMENT = "DECREMENT" as const;
 
@@ -25,9 +25,20 @@ type CounterAction = IncrementAction | DecrementAction;
 
 
 
-// 리듀서
-const counter = (state = initialState, action:CounterAction) => {
+// 리듀서 -> 액션 타입에 따라 상태 값을 어떻게 리턴 해줄지 정하는 함수.
+const counter = (state = initialState, action: CounterAction) => {
+  console.log(action)
   switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state, // 기존 상태를 복사
+        number: state.number + 1, // number 값을 1 증가
+      };
+    case DECREMENT:
+      return {
+        ...state, // 기존 상태를 복사
+        number: state.number - 1, // number 값을 1 감소
+      };
     default:
       return state;
   }
