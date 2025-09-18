b01849332f2a7af371cb4db4b96be39ee53baed8
parksubeom <sooknise@naver.com>
2025-09-18 18:08:50 +0900

    refactor : 카운터 프로그램 -> 투두리스트로 리팩터링

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
 import { useSelector,useDispatch } from "react-redux";
 import type { RootState,AppDispatch } from "./redux/config/configStore";
-import { increment, decrement } from "./redux/modules/counter";
+import { addTodo, deleteTodo } from "./redux/modules/counter";
 import type { ChangeEvent} from "react";
 import { useState } from "react";
 
 const App = () => {
+
+  interface TodoListType {
+  title: string,
+  author: string,
+  date: string,
+  content: string,
+ 
+}
 
   /**
   리듀서로부터 상태값을 받아오는 훅
   리듀서의 여러 상태 중 number만 가져옴
   number 상태를 공유하는 페이지만 리렌더링이 발생하여 useContext에 비해 최적화
    */
-  const [number, setNumber] = useState<number>(0);
-  const counterStore = useSelector((state: RootState) => state.counter.number)
+  const [todoList, setTodoList] = useState<TodoListType>({
+  title: "",
+  author: "",
+  date: "",
+  content: "",
+ 
+});
+  const counterStore = useSelector((state: RootState) => state.counter.todoContent)
   const dispatch = useDispatch<AppDispatch>(); // 액션을 리듀서로 보내주는 파이프 
   const onChangeValue = (e:ChangeEvent<HTMLInputElement>) => {
     const value = +e.target.value
-    setNumber(value)
   }
 
   return(
     <>
-    <input type="number" onChange={onChangeValue}></input>
-    <button onClick={() => dispatch(increment(number))}>+</button>
-    <button onClick={() => dispatch(decrement(number))}>-</button>
-    <div>Count : {counterStore}</div>
+    <input type="text" onChange={onChangeValue}></input>
     </>
   ) 
   
--- src/redux/modules/counter.ts
+++ src/redux/modules/counter.ts
@@ -2,61 +2,68 @@
 
 // 초기 상태값 -> 카운터를 만들거기 떄문에 값의 타입은 넘버
 const initialState = {
-  number: 0,
+  todoContent: [
+    {
+      title: "나는 첫 일정이다.",
+      author: "박수범이다",
+      date: "2025.09.18",
+      content: "아무것도 안하는거다",
+    },
+  ],
 };
 
 // 액션 타입 상수를 정의합니다.
 // `as const`를 사용하면 타입이 string에서 리터럴로 변경 -> 리터럴이 아니면 string 타입도 추론으로 허용되어버림
-const INCREMENT = "INCREMENT" as const; 
-const DECREMENT = "DECREMENT" as const;
+const ADDTODO = "ADDTODO" as const; 
+const DELELTETODO = "DELETETODO" as const;
 
 // 1. 증가 액션 타입 정의
-interface IncrementAction {
-  type: typeof INCREMENT;
-  payload:number
+interface AddTodo {
+  type: typeof ADDTODO;
+  payload:{}
 }
 // 2. 감소 액션 타입 정의
-interface DecrementAction {
-  type: typeof DECREMENT;
-  payload:number
+interface DeleteTodo {
+  type: typeof DELELTETODO;
+  payload:{}
 }
 
 
-export const increment = (value:number) => {
+export const addTodo = (value:{}) => {
   return {
-    type: INCREMENT,
+    type: ADDTODO,
     payload:value
   };
 };
 
-export const decrement = (value:number) => {
+export const deleteTodo = (value:{}) => {
   return {
-    type: DECREMENT,
+    type: DELELTETODO,
     payload:value
   };
 };
 
 
 // 3. 액션타입 유니온으로 묶기
-type CounterAction = IncrementAction | DecrementAction;
+type TodoAction = AddTodo | DeleteTodo;
 
 
 
 // 리듀서 -> 액션 타입에 따라 상태 값을 어떻게 리턴 해줄지 정하는 함수.
-const counter = (state = initialState, action: CounterAction) => {
+const counter = (state = initialState, action: TodoAction) => {
  
   switch (action.type) {
-    case INCREMENT:
+    case ADDTODO:
       console.log(action.payload)
       return {
         ...state, // 불변성 유지 -> 다음주에 RTK 사용 시 대체 가능
-        number: state.number + action.payload, 
+        todoContent : [...state.todoContent,action.payload]
       };
-    case DECREMENT:
+    case DELELTETODO:
       console.log(action.payload)
       return {
         ...state, // 불변성 유지 -> 다음주에 RTK 사용 시 대체 가능
-        number: state.number - action.payload, 
+        todoContent : state.todoContent.filter((el) => el === action.payload)
       };
     default:
       return state;
