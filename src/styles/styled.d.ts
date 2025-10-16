7ccabec3230e4e5e5a95476941457e321c3deea1
Bumpist <104641096+parksubeom@users.noreply.github.com>
2025-10-16 23:59:23 +0900

    fix

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,7 +1,8 @@
 // src/styles/styled.d.ts
 
 import 'styled-components';
-import { ThemeType } from './theme';
+// 이 경로('./theme')와 ThemeType의 export 이름이 일치해야 합니다.
+import { ThemeType } from './theme'; 
 
 declare module 'styled-components' {
   // export 한 ThemeType을 DefaultTheme에 확장합니다.
